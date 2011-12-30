1258782ad12f450d5c745ceb47b7d9dfa6af1d96
Robert Carr <robert@rightscale.com>
2011-12-30 14:32:15 -0800

    Merged cs_api.rb into cloudreport.rb

--- cloudreport.rb
+++ cloudreport.rb
@@ -8,21 +8,66 @@
 # generates a pretty report using XSLT to transform 
 # the resulting XML from the API calls.
 #
-# requires: cs_api.rb to handle the signed api request.
 #
 
+require 'rubygems'
 require 'nokogiri'
+require 'openssl'
+require 'base64'
+require 'cgi'
+require 'yaml'
+
+begin
+config = YAML::parse(File.open("creds.yml")).transform
+rescue
+puts "Can't find creds.yml file - Please create first"
+exit
+end
+
+# API calls that will get executed against the endpoint
+# Some of these commands require you to have DOMAIN ADMIN priviledges!
 
 api_calls = %w{ 
-                  listPods listCapabilities listDomains listZones listHypervisors listServiceOfferings
+                  listCapabilities listDomains listZones listHypervisors listServiceOfferings
                   listDiskOfferings listNetworkOfferings listTemplates\&templatefilter=community\&name=right 
               }
 
+
+# Credentials & Endpoint are read from ./creds.yml
+@CLOUD_API_KEY        = config['cloud_api_key']
+@CLOUD_API_SECRET     = config['cloud_api_secret']
+@CLOUD_API_ENDPOINT   = config['cloud_api_endpoint']
+
+# Build a CloudStack query string and sign it.
+#
+def query(string)
+# TODO: url encode values for each keypair prior to signing.  
+# TODO: Urlencode '+' w/ '%20'
+  pre_signed = "apiKey=#{@CLOUD_API_KEY}&" + "command=" + string.split("&").sort.join("&")
+  sign(@CLOUD_API_SECRET, pre_signed)
+end
+
+# Return Signature
+#
+def sign(secret,string)
+  CGI.escape(Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::SHA1.new, secret, string.downcase)).chomp)
+end
+
+def callapi(command)
+# TODO: Send command & check for success response.  Add retry?
+  cmd="#{@CLOUD_API_ENDPOINT}" + "command=" + command + "&apiKey=#{@CLOUD_API_KEY}" + "&signature=#{query(command)}"
+  res_code=`curl -sw %{http_code} "#{cmd}"`
+  res_code   unless res_code.match("does not exist")
+end
+
 def dumpxml(command)
-  rawxml = `./cs_api.rb command=#{command}`
+  rawxml = callapi(command)
   xml = Nokogiri::XML(rawxml)
   xslt = Nokogiri::XSLT(File.read('./cloudstack.xslt'))
   puts xslt.transform(xml)
 end
 
-api_calls.each { |x| dumpxml(x) }
+
+# Cycle through each of the API calls.
+# Build a properly formatted API string, sign it, call it & transform the results.
+api_calls.each { |cmd| dumpxml(cmd) }
